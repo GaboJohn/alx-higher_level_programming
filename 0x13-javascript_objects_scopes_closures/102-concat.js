336e84c4022abd95e1b5c81f10a4329a39b9480e
GaboJohn <johngabotsalelwe@gmail.com>
2024-02-13 02:07:21 -0800

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,3 @@ const fs = require('fs');
 const firstArg = fs.readFileSync(process.argv[2]).toString();
 const secondArg = fs.readFileSync(process.argv[3]).toString();
 fs.writeFileSync(process.argv[4], firstArg + secondArg);
-
