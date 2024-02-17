ea0efadb90d3d19e2072a4ce8d8e8c3796c1c6c6
GaboJohn <johngabotsalelwe@gmail.com>
2024-02-17 10:57:53 -0800

    Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for x in session.query(State).filter(State.name.like('%a%')):
-        print(x.id, instance.name, sep=": ")
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
