2a399a53cb8ecda1e2b6042ec864c509ed164eec
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-01 09:28:33 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,5 @@
 # Sends a request to a URL and displays size in bytes
 
 URL="$1"
-SIZE=$(curl -sI "$URL" | grep -i "Content-Length" | awk '{print $2}')
-echo "$SIZE"
+curl -sI "$URL" | grep -i "Content-Length" | awk '{print $2}'
+
