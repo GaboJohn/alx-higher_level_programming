8d5b5ca21b222274f86802292bc73e678b4b090a
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-03 12:15:37 -0800

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response for a 200 status code
-
-URL="$1"
-curl -s -L "$URL"
-
+# Sends a GET request to URL and displays the body of response
+curl -sL "$1"
