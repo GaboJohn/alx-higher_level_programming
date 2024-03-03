f0184e1010947ebdbb6d0ebaabac50ebd95b8774
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-03 12:29:54 -0800

    cURL Method

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays size in bytes
-
 curl -sI "$1" | grep -i 'Content-Length' | cut -d ' ' -f2
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # A delete request to the Url passed as first argument
-
 curl -sX DELETE "$1"
