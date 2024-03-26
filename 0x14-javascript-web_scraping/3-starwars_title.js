fb592cb3ee804f5ec7fa88ed4ac1f074aa6ccddf
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-26 04:43:33 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-let request = require('request');
-let episodeNum = process.argv[2];
-let API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    let responseJSON = JSON.parse(body);
+    const responseJSON = JSON.parse(body);
     console.log(responseJSON.title);
   } else {
     console.log('Error code: ' + response.statusCode);
